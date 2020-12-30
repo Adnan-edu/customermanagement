889c243bdcff46816f46b2ebfc800270a104b02e
Adnan-edu <md.ashikadnan@gmail.com>
2020-12-31 04:09:34 +1100

    Added model form, performed crud operation and added styles

--- crmproject/accounts/views.py
+++ crmproject/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 # Create your views here.
 
 
@@ -35,3 +36,37 @@ def customer(request,pk_test):
     orders_count = orders.count()
     context = {'customer':customer,'orders':orders,'orders_count':orders_count}
     return render(request,'accounts/customer.html',context)
+
+def createOrder(request):
+	form = OrderForm()
+	if request.method == 'POST':
+		form = OrderForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)#Item instances we are going to fill out in this form
+
+	if request.method == 'POST':
+		form = OrderForm(request.POST, instance=order) #We are posting new post data into the previous instance inorder to avoid creating a new instance again
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method == "POST":
+		order.delete()
+		return redirect('/')
+
+	context = {'item':order}
+	return render(request, 'accounts/delete.html', context)
