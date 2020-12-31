63e5d731bf9bcc873b99c2d036d745c26d069a92
Adnan-edu <md.ashikadnan@gmail.com>
2021-01-01 01:51:10 +1100

    User Registration and Login Authentication

--- crmproject/accounts/views.py
+++ crmproject/accounts/views.py
@@ -3,13 +3,64 @@ from django.http import HttpResponse
 from .models import *
 #To create multiple form within one form
 from django.forms import inlineformset_factory
-from .forms import OrderForm
-from .filters import OrderFilter
+from .forms import OrderForm, CreateUserForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
 
+				return redirect('login')
+			
 
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+def userPage(request):
+	context = {}
+	return render(request, 'accounts/user.html', context)	
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -28,12 +79,14 @@ def home(request):
     }
     return render(request,'accounts/dashboard.html',context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request,'accounts/products.html',{
         'products':products
     })
 
+@login_required(login_url='login')
 def customer(request,pk_test):
     customer = Customer.objects.get(id=pk_test)
     #Filter-We are first going to query all the orders.
@@ -48,6 +101,7 @@ def customer(request,pk_test):
     ,'myFilter':myFilter}
     return render(request,'accounts/customer.html',context)
 
+@login_required(login_url='login')
 def createOrder(request,pk):
     OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
     customer = Customer.objects.get(id=pk)
@@ -63,6 +117,7 @@ def createOrder(request,pk):
     context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def updateOrder(request, pk):
 
 	order = Order.objects.get(id=pk)
@@ -77,6 +132,7 @@ def updateOrder(request, pk):
 	context = {'form':form}
 	return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
 	order = Order.objects.get(id=pk)
 	if request.method == "POST":
