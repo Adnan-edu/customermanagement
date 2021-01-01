ec28e7d4a8c6103fae987c347062123af4d26077
Adnan-edu <md.ashikadnan@gmail.com>
2021-01-02 01:31:16 +1100

    Changed

--- crmproject/crmproject/settings.py
+++ crmproject/crmproject/settings.py
@@ -136,4 +136,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'adnan.cs.ml@gmail.com'
-EMAIL_HOST_PASSWORD = '1234Adnan_csE1234'
+EMAIL_HOST_PASSWORD = '********'
